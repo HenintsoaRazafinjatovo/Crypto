b0f9714bd4ec415e3aaeb125d37492e5733baa97
HenintsoaRazafinjatovo <henintsoarazafinjatovo@gmail.com>
2024-12-20 14:44:17 +0300

    commiteko fotsiny de testeo any

---
+++ src/main/java/mg/crypto/controllers/TestController.java
@@ -0,0 +1,12 @@
+package mg.crypto.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home()
+    {
+        return "home";
+    }
+}
